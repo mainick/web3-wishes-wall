8ed8cb7c9483cd5e906f682ac533a35d40fd04fe
mainick <maico.orazio@gmail.com>
2022-04-25 12:25:09 +0200

    added the ability to vote the wish

--- smart-contract/contracts/WishesWall.sol
+++ smart-contract/contracts/WishesWall.sol
@@ -8,15 +8,21 @@ contract WishesWall {
     struct Wish {
         address owner;
         string message;
-        uint256 timestamp;
+        uint32 timestamp;
+        uint32 voteSum;
+        uint16 voteCount;
     }
 
-    uint256 totalWishes;
+    uint32 totalWishes;
     Wish[] wishes;
     uint256 private _seed;
-    mapping(address => uint256) public lastWishAt;
+    mapping(address => uint32) public lastWishAt;
 
-    event NewWish(address indexed from, uint256 timestamp, string message);
+    uint32 totalVotes;
+    mapping(uint32 => address) public voters;
+
+    event NewWish(uint32 id, address indexed from, uint32 timestamp, string message);
+    event NewVote(address indexed from, uint32 timestamp, uint8 vote);
 
     constructor() payable {
         console.log("Hey, I'm the smart contract of the wall of wishes!");
@@ -25,29 +31,17 @@ contract WishesWall {
 
     function wish(string memory _message) public {
         require(lastWishAt[msg.sender] + 15 minutes < block.timestamp, "You can't make a wish more than once every 15 minutes!");
-        lastWishAt[msg.sender] = block.timestamp;
+        lastWishAt[msg.sender] = uint32(block.timestamp);
 
-        totalWishes++;
-        console.log("%s has sent a wish %s", msg.sender, _message);
+        uint32 wishId = totalWishes++;
+        console.log("%s has sent a wish [%d] '%s' ", msg.sender, wishId, _message);
 
-        wishes.push(Wish(msg.sender, _message, block.timestamp));
+        wishes.push(Wish(msg.sender, _message, uint32(block.timestamp), 0, 0));
 
-        _seed = (_seed + block.timestamp + block.difficulty) % 100;
-        console.log("Random # generated: %d", _seed);
-        if (_seed <= 50) {
-            uint256 prizeAmount = 0.0001 ether;
-            require(prizeAmount <= address(this).balance, "You don't have enough ether to send a wish!");
-
-            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-            require(success, "Failed to withdraw money from contract.");
-
-            console.log("%s has won %d ether!", msg.sender, prizeAmount);
-        }
-
-        emit NewWish(msg.sender, block.timestamp, _message);
+        emit NewWish(wishId, msg.sender, uint32(block.timestamp), _message);
     }
 
-    function getTotalWishes() public view returns (uint256) {
+    function getTotalWishes() public view returns (uint32) {
         console.log("We've %d total wishes!", totalWishes);
         return totalWishes;
     }
@@ -57,5 +51,52 @@ contract WishesWall {
             console.log("%s wishes: %s", wishes[i].owner, wishes[i].message);
         }
         return wishes;
+    }
+
+    function getAverageRating() public view returns (uint8) {
+        uint256 sum = 0;
+        for (uint i = 0; i < totalWishes; i++) {
+            sum += wishes[i].voteSum;
+        }
+        return uint8(sum / totalVotes);
+    }
+
+    function getAverageRatingOfWish(uint32 _wishId) public view returns (uint8) {
+        return uint8(wishes[_wishId].voteSum / wishes[_wishId].voteCount);
+    }
+
+    function vote(uint32 _wishId, uint8 _rate) public {
+        require(0 <= _rate && _rate <= 5, "Rate must be between 0 and 5!");
+        require(voters[_wishId] != msg.sender, "You can't vote twice for the same wish!");
+
+        wishes[_wishId].voteSum += _rate;
+        wishes[_wishId].voteCount++;
+        totalVotes++;
+        voters[_wishId] = msg.sender;
+        console.log("%s has voted %d for wish %d", msg.sender, _rate, _wishId);
+
+        _seed = (_seed + block.timestamp + block.difficulty) % 100;
+        console.log("Random # generated: %d", _seed);
+        if (_seed <= 50) {
+            uint256 prizeAmount = 0.00001 ether;
+            require(prizeAmount <= address(this).balance, "Contract don't have enough ether to send a wish!");
+
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+
+            console.log("%s has won %d ether!", msg.sender, prizeAmount);
+        }
+
+        emit NewVote(msg.sender, uint32(block.timestamp), _rate);
+    }
+
+    function getTotalVotes() public view returns (uint32) {
+        console.log("We've %d total votes!", totalVotes);
+        return totalVotes;
+    }
+
+    function getTotalVotesOfWish(uint32 _wishId) public view returns (uint16) {
+        console.log("Wish %d received %d votes", _wishId, wishes[_wishId].voteCount);
+        return wishes[_wishId].voteCount;
     }
 }
--- smart-contract/scripts/run.js
+++ smart-contract/scripts/run.js
@@ -1,3 +1,7 @@
+const getContractBalance = async (contract) => {
+  return await hre.ethers.provider.getBalance(contract.address);
+};
+
 const main = async () => {
   const [owner, randomPerson] = await hre.ethers.getSigners();
 
@@ -9,9 +13,7 @@ const main = async () => {
   console.log("WishesWall contract address: ", wishContract.address);
   console.log("Contract deployed by", owner.address);
 
-  let contractBalance = await hre.ethers.provider.getBalance(
-    wishContract.address
-  );
+  let contractBalance = await getContractBalance(wishContract);
   console.log(
     "Contract balance: ",
     hre.ethers.utils.formatEther(contractBalance)
@@ -19,37 +21,94 @@ const main = async () => {
 
   let wishCount;
   wishCount = await wishContract.getTotalWishes();
-  console.log("Total wishes: ", wishCount.toNumber());
+  console.log("Total wishes: ", wishCount);
 
-  let wishTxn = await wishContract.wish("first wish");
+  let wishTxn = await wishContract.wish("first wish", {
+    gasLimit: 210000,
+  });
   await wishTxn.wait();
 
   wishCount = await wishContract.getTotalWishes();
-  console.log("Total wishes: ", wishCount.toNumber());
+  console.log("Total wishes: ", wishCount);
 
-  contractBalance = await hre.ethers.provider.getBalance(wishContract.address);
-  console.log(
-    "Contract balance: ",
-    hre.ethers.utils.formatEther(contractBalance)
-  );
-
-  wishTxn = await wishContract.connect(randomPerson).wish("second wish");
+  wishTxn = await wishContract.connect(randomPerson).wish("second wish", {
+    gasLimit: 210000,
+  });
   await wishTxn.wait();
 
   wishCount = await wishContract.getTotalWishes();
-  console.log("Total wishes: ", wishCount.toNumber());
-
-  wishTxn = await wishContract.connect(randomPerson).wish("third wish");
-  await wishTxn.wait();
-
-  contractBalance = await hre.ethers.provider.getBalance(wishContract.address);
-  console.log(
-    "Contract balance: ",
-    hre.ethers.utils.formatEther(contractBalance)
-  );
+  console.log("Total wishes: ", wishCount);
 
   const wishesAll = await wishContract.getAllWishes();
   console.log("Wishes: ", wishesAll);
+
+  let voteCount;
+  voteCount = await wishContract.getTotalVotes();
+  console.log("Total votes: ", voteCount);
+
+  let voteCountWish;
+  voteCountWish = await wishContract.getTotalVotesOfWish(0);
+  console.log("Total votes for wish 0: ", voteCountWish);
+
+  let voteTnx = await wishContract.vote(0, 2, {
+    gasLimit: 210000,
+  });
+  await voteTnx.wait();
+
+  contractBalance = await getContractBalance(wishContract);
+  console.log(
+    "Contract balance: ",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  voteCount = await wishContract.getTotalVotes();
+  console.log("Total votes: ", voteCount);
+
+  voteCountWish = await wishContract.getTotalVotesOfWish(0);
+  console.log("Total votes for wish 0: ", voteCountWish);
+
+  voteTnx = await wishContract.connect(randomPerson).vote(0, 5, {
+    gasLimit: 210000,
+  });
+  await voteTnx.wait();
+
+  contractBalance = await getContractBalance(wishContract);
+  console.log(
+    "Contract balance: ",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  voteTnx = await wishContract.connect(randomPerson).vote(1, 4, {
+    gasLimit: 210000,
+  });
+  await voteTnx.wait();
+
+  contractBalance = await getContractBalance(wishContract);
+  console.log(
+    "Contract balance: ",
+    hre.ethers.utils.formatEther(contractBalance)
+  );
+
+  voteCount = await wishContract.getTotalVotes();
+  console.log("Total votes: ", voteCount);
+
+  voteCountWish = await wishContract.getTotalVotesOfWish(0);
+  console.log("Total votes for wish 0: ", voteCountWish);
+
+  voteCountWish = await wishContract.getTotalVotesOfWish(1);
+  console.log("Total votes for wish 1: ", voteCountWish);
+
+  const wishesAll2 = await wishContract.getAllWishes();
+  console.log("Wishes: ", wishesAll2);
+
+  let avgRating = await wishContract.getAverageRating();
+  console.log("average rating: ", avgRating);
+
+  let avgRatingWish = await wishContract.getAverageRatingOfWish(0);
+  console.log("average rating of wish 0: ", avgRatingWish);
+
+  avgRatingWish = await wishContract.getAverageRatingOfWish(1);
+  console.log("average rating of wish 1: ", avgRatingWish);
 };
 
 const runMain = async () => {
